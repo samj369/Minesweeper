92741ec5766760ee619f88779dae2a2fe5d20882
samj369 <sjai563@aucklanduni.ac.nz>
2024-03-11 23:22:56 +1300

    added def test_play_turn():

--- test_minesweeper.py
+++ test_minesweeper.py
@@ -15,12 +15,12 @@ def test_insert_mines():
     board = ["O"] * 25
     positions = [(0,0),(1,1),(2,2),(3,2),(0,4)]
     insert_mines(board,positions)
-    assert board == ['X','O','O','O','X',
+    assert (board == ['X','O','O','O','X',
                      'O','X','O','O','O',
                      'O','O','X','O','O',
                      'O','O','X','O','O',
                      'O','O','O','O','O',
-                     ]
+                     ])
 
 def test_count_adjacent_mines():
     board = ['O', 'O', 'O', 'O', 'O',
@@ -29,10 +29,36 @@ def test_count_adjacent_mines():
              'O', 'X', 'X', 'X', 'O',
              'O', 'O', 'O', 'O', 'O',
              ]
-    assert count_adjacent_mines(board,0,1) == 2
     assert count_adjacent_mines(board,2,2) == 8
-    assert count_adjacent_mines(board, 4, 4) == 1
-    assert count_adjacent_mines(board, 2, 4) == 3
+
+
+def test_play_turn():
+    board = ['X', 'O', 'O', 'O', 'O',
+             'O', 'O', 'O', 'O', 'O',
+             'O', 'O', 'O', 'O', 'O',
+             'O', 'O', 'O', 'O', 'O',
+             'O', 'O', 'O', 'O', 'O',
+             ]
+    row = 0
+    column = 0
+    mod_board, mine_hit = play_turn(board,row,column)
+    assert mine_hit == True
+
+
+def test_check_win():
+    board = ['X', '2', '1', ' ', ' ',
+             '2', 'X', '2', '1', ' ',
+             '1', '2', 'X', '2', ' ',
+             ' ', '1', '2', 'X', '2',
+             ' ', ' ', '1', '2', 'X',
+             ]
+    game_over = check_win(board)
+    assert (game_over == True)
+
+
+
+
+
 
 
 
