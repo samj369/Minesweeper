5cc1e2d816ef3c01788050fe2cbd4ab50d41fe51
samj369 <sjai563@aucklanduni.ac.nz>
2024-03-11 22:03:32 +1300

    added def test_insert_mines():

--- test_minesweeper.py
+++ test_minesweeper.py
@@ -9,3 +9,14 @@ def test_count_adjacent_mines_in_corner():
 
 count = count_adjacent_mines(board,0,4)
 assert(count == 0)
+
+def test_insert_mines():
+    board = ["O"] * 25
+    positions = [(0,0),(1,1),(2,2),(3,2),(0,4)]
+    insert_mines(board,positions)
+    assert board == ['X','O','O','O','X',
+                     'O','X','O','O','O',
+                     'O','O','X','O','O',
+                     'O','O','X','O','O',
+                     'O','O','O','O','O',
+                     ]
